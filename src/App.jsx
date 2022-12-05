061c172d79dcd932caeb0a0703b49a2071bae59a
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2022-12-05 14:01:55 +0700

    finish section 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
+import AnimalShow from './components/AnimalShow.jsx'
+
+function getRandomAnimal() {
+	const animals = ['dog', 'cat', 'chicken', 'cow', 'horse', 'bird']
+
+	return animals[Math.floor(Math.random() * animals.length)]
+}
+
 function App() {
 
-  return (
-    <div>
-      <h1 className='p-6 font-bold text-3xl'>Hello React + Redux + Tailwind + Vite!</h1>
-    </div>
-  )
+	const [animals, setAnimals] = useState([])
+
+	const handleClick = () => {
+		setAnimals([...animals, getRandomAnimal()])
+	}
+
+	const renderedAnimals = animals.map((animal, index) => {
+		return <AnimalShow type={animal.toUpperCase()} key={index}/>
+	})
+
+	return (
+		<div className='p-6'>
+			<h1 className='font-bold text-3xl'>Hello React + Redux + Tailwind + Vite!</h1>
+			<div className='pt-6'>
+				<button onClick={handleClick} className='p-2 bg-blue-400 border-2 border-black'>Click here</button>
+				<p>{renderedAnimals}</p>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/AnimalShow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AnimalShow({ type }) {
+	return (
+		<>
+			<p>{type}</p>
+		</>
+	)
+}
+
+export default AnimalShow
