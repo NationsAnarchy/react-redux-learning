e57e8997321c4563036c558008036b963c2257f8
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2022-12-05 16:28:57 +0700

    update styling on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
 
-  return (
-    <div>
-      <h1 className='p-6 font-bold text-3xl'>Hello React + Redux + Tailwind + Vite!</h1>
-    </div>
-  )
+	return (
+		<div className='p-6'>
+			<h1 className='font-bold text-3xl'>Hello React + Redux + Tailwind + Vite!</h1>
+		</div>
+	)
 }
 
 export default App
